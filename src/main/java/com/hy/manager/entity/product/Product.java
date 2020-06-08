84d7b515896b73eb1a4bd9648ce2fce9b87a46ea
qhf <2817160162@qq.com>
2020-06-08 17:45:48 +0800

    2020-6-8下午(齐海峰)提交

--- src/main/java/com/hy/manager/entity/product/Product.java
+++ src/main/java/com/hy/manager/entity/product/Product.java
@@ -61,5 +61,143 @@ public class Product implements Serializable {
 
     private Integer isShow;
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProductNumber() {
+        return productNumber;
+    }
+
+    public void setProductNumber(String productNumber) {
+        this.productNumber = productNumber;
+    }
+
+    public Integer getSellVolume() {
+        return sellVolume;
+    }
+
+    public void setSellVolume(Integer sellVolume) {
+        this.sellVolume = sellVolume;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getUpStatus() {
+        return upStatus;
+    }
+
+    public void setUpStatus(Integer upStatus) {
+        this.upStatus = upStatus;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getProductModel() {
+        return productModel;
+    }
+
+    public void setProductModel(String productModel) {
+        this.productModel = productModel;
+    }
+
+    public String getProductColor() {
+        return productColor;
+    }
+
+    public void setProductColor(String productColor) {
+        this.productColor = productColor;
+    }
+
+    public Date getMarketDate() {
+        return marketDate;
+    }
+
+    public void setMarketDate(Date marketDate) {
+        this.marketDate = marketDate;
+    }
+
+    public String getProductMaterials() {
+        return productMaterials;
+    }
+
+    public void setProductMaterials(String productMaterials) {
+        this.productMaterials = productMaterials;
+    }
+
+    public String getProductUser() {
+        return productUser;
+    }
+
+    public void setProductUser(String productUser) {
+        this.productUser = productUser;
+    }
+
+    public Integer getImages() {
+        return images;
+    }
+
+    public void setImages(Integer images) {
+        this.images = images;
+    }
+
+    public Integer getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Integer classes) {
+        this.classes = classes;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public Integer getIsShow() {
+        return isShow;
+    }
+
+    public void setIsShow(Integer isShow) {
+        this.isShow = isShow;
+    }
 }
